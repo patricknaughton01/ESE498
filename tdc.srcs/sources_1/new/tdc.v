64249b2caaa463a07a324207ba71a834ad75ad8a
DESKTOP-2GDKRNR\ressw <resswein@wustl.edu>
2020-02-13 21:28:48 -0600

    Carry4 things

--- tdc.srcs/sources_1/new/tdc.v
+++ tdc.srcs/sources_1/new/tdc.v
@@ -27,28 +27,40 @@ module tdc#(parameter INITIAL=64, DELAY=64)(
     output  reg[DELAY-1:0]    delay
 );
 
-(* dont_touch = "true" *) wire[INITIAL-1:0] initial_bufs;
-(* dont_touch = "true" *) wire[DELAY-1:0] delay_bufs;
+(* dont_touch = "true" *) wire[INITIAL:0] initial_bufs;     // Not INITIAL-1 so that instantiation is convenient
+(* dont_touch = "true" *) wire[DELAY:0] delay_bufs;
 reg[DELAY-1:0] latches;
 reg[DELAY-1:0] delayD;
 
 generate
     genvar k;
-    for(k = 1; k < INITIAL; k = k + 1)begin
-        (* dont_touch = "true" *) buffer init(initial_bufs[k], initial_bufs[k-1]);
+    for(k = 0; k < INITIAL/4; k = k + 1)begin
+        CARRY4 init_k (
+            .DI(initial_bufs[k*4+3:k*4]),
+            .S('b0000),
+            .CYINIT('b0),
+            .CI('b0),
+            .O(initial_bufs[k*4+4:k*4+1])
+        );
     end
-    for(k = 1; k < DELAY; k = k + 1)begin
-        (* dont_touch = "true" *) buffer delay(delay_bufs[k], delay_bufs[k-1]);
+    for(k = 0; k < DELAY/4; k = k + 1)begin
+         CARRY4 delay_k (
+            .DI(delay_bufs[k*4+3:k*4]),
+            .S('b0000),
+            .CYINIT('b0),
+            .CI('b0),
+            .O(delay_bufs[k*4+4:k*4+1])
+        );
     end
 endgenerate
 
 assign initial_bufs[0] = clk;
-assign delay_bufs[0] = initial_bufs[INITIAL-1];
+assign delay_bufs[0] = initial_bufs[INITIAL];
 
 integer i;
 always @ * begin
     if(clk)begin
-        for(i = 0; i<DELAY; i = i + 1)begin
+        for(i = 0; i<=DELAY; i = i + 1)begin
             latches[i] = delay_bufs[i];
         end
     end
