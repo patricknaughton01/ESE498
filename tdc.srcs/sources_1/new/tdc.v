c07f946d748ea96cca1d962dd51d24b488241984
DESKTOP-2GDKRNR\ressw <resswein@wustl.edu>
2020-02-14 16:11:36 -0600

    Fixed Carry4 things, I think

--- tdc.srcs/sources_1/new/tdc.v
+++ tdc.srcs/sources_1/new/tdc.v
@@ -24,38 +24,32 @@ module tdc#(parameter INITIAL=64, DELAY=64)(
     input               clk,
     input               reset,
     // TODO: Replace this with an encoder that just uses 6 bits
-    output  reg[DELAY-1:0]    delay
+    output  reg[DELAY:0]    delay
 );
 
-(* dont_touch = "true" *) wire[INITIAL:0] initial_bufs;     // Not INITIAL-1 so that instantiation is convenient
+(* dont_touch = "true" *) wire[INITIAL-1:0] initial_bufs;     // Not INITIAL-1 so that instantiation is convenient
 (* dont_touch = "true" *) wire[DELAY:0] delay_bufs;
-reg[DELAY-1:0] latches;
-reg[DELAY-1:0] delayD;
+reg[DELAY:0] latches;
+reg[DELAY:0] delayD;
 
 generate
     genvar k;
-    for(k = 0; k < INITIAL/4; k = k + 1)begin
-        CARRY4 init_k (
-            .DI(initial_bufs[k*4+3:k*4]),
-            .S('b0000),
-            .CYINIT('b0),
-            .CI('b0),
-            .O(initial_bufs[k*4+4:k*4+1])
-        );
+    for(k = 1; k < INITIAL; k = k + 1)begin
+        (* dont_touch = "true" *) buffer init(initial_bufs[k], initial_bufs[k-1]);
     end
     for(k = 0; k < DELAY/4; k = k + 1)begin
-         CARRY4 delay_k (
+         (* dont_touch = "true" *) CARRY4 delay_k (
             .DI(delay_bufs[k*4+3:k*4]),
             .S('b0000),
             .CYINIT('b0),
             .CI('b0),
-            .O(delay_bufs[k*4+4:k*4+1])
+            .CO(delay_bufs[k*4+4:k*4+1])
         );
     end
 endgenerate
 
 assign initial_bufs[0] = clk;
-assign delay_bufs[0] = initial_bufs[INITIAL];
+assign delay_bufs[0] = initial_bufs[INITIAL-1];
 
 integer i;
 always @ * begin
